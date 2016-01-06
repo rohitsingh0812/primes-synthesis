e189df4b03cdc04d3e98a963bd9b69ba66d09dec
Justin <jkaashoek@users.noreply.github.com>
2016-01-05 19:31:05 -0500

    Update Derivative.py

--- Derivative.py
+++ Derivative.py
@@ -1,9 +1,10 @@
 import math
 
 class Specification:
-    dict bounds, params
+    bounds={}
+    params={}
 
-    def __init__():
+    def __init__(self):
         return
     
     def template(self):
@@ -25,9 +26,9 @@ class Specification:
         return
 
 
-class PolynomialDerivative:
+class PolynomialDerivative(Specification):
 
-    def __init__():
+    def __init__(self):
         params = {'x':0, 'p':0, 'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0}
         bounds = {'x': 100, 'p': 5, 'a': 9, 'b': 9, 'c': 9, 'd': 9, 'e': 9, 'f': 9}
             
@@ -37,47 +38,46 @@ class PolynomialDerivative:
         for i in range(params['p']):
             if params[i] != 0:
                 print params[i] + "x^" + values[-1] + " + "
-        print values[5] + "x^" + values[-1] " at x = " params[x]
+        print values[5] + "x^" + values[-1] + " at x = " + params[x]
 
     def constraints(self):
         for key in params.keys():
             if abs(params[key]) > bounds[key]:
                 return False
         return True
-        
+
     def compute(self):
-         userAns = input("Your solution: ")
-         while not isCorrect(userAns):
-             userAns = input("Incorrect! Try again: ")
+        userAns = input("Your solution: ")
+        while not isCorrect(userAns):
+            userAns = input("Incorrect! Try again: ")
         print "Correct!"
 
     def isCorrect(userAns):
-        if calcDeriv() == userAns && constraints():
+        if calcDeriv() == userAns and constraints():
             return True
         return False
         
     def calcDeriv():
-        int deriv = 0, count=1
+        deriv = 0
+        count = 1
         for i in range(97, 97+params['p']):
             if params[chr(i)] != 0:
                 deriv += ((count)*params[chr(i)]) * (params['x']**(count-1))
-            count ++
-        
-            
-        deriv += params['p']
+            count += 1
 
 class Solver:
 
-    Specification spec
+    spec = Specification()
     
     def __init__(self, specification):
         spec = specification
 
     def setProblemParams(self):
-        dict params, bounds = spec.bounds
+        params={}
+        bounds = Solver.spec.bounds
 
         for key in bounds.keys():
-            int val = randint(1, bounds[key])
+            val = randint(1, bounds[key])
             if randint(1,2) == 1:
                 val *= -1
             params[key] = val
@@ -85,8 +85,8 @@ class Solver:
                  
     
 
-PolynomialDerivative p
-Solver s
-s.setProblemparams()
+p = PolynomialDerivative()
+s = Solver(p)
+s.setProblemParams()
 print p.template()
         
